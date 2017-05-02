8cbaf61b283143e83270154c887925dddd401a3c
lamfalusy <lamfalusy@gmail.com>
2017-05-02 22:03:24 +0200

    init repo

---
+++ src/main/java/hu/lamsoft/experiment/aop/AopApplication.java
@@ -0,0 +1,14 @@
+package hu.lamsoft.experiment.aop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+	
+}
